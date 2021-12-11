852f027ed2b24c3aabba2c030b823a00d05e8648
Gabrielle <gabinunes09@gmail.com>
2021-12-10 23:37:49 -0300

    Inserindo spring fox e swagger para documentação

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableCaching
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableSwagger2
 public class ForumApplication {
 
 	public static void main(String[] args) {
