cfaea9892470aa7e57b0c3a7bc5d678f49b6544a
Gabrielle <gabinunes09@gmail.com>
2021-12-11 22:04:20 -0300

    adicionando recursos de profiles

---
+++ src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
@@ -0,0 +1,26 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("dev")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	
+	//Configuração de autorização (perfil/nivel de acesso e etc)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/**").permitAll()
+		.anyRequest().authenticated().and().csrf().disable();
+		
+	}
+	
+}
